4b683744f45564372efa0b8886fcfba3782f2e3f
Gerald Todd <gerald@gtodd.dev>
2020-07-17 17:13:57 -0700

    Add private method allowing article parameters and define standard CRUD actions.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,31 @@
 class ArticlesController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+
     def new
     end
 
-    def create
-        render plain: params[:article].inspect
+    def edit
     end
+
+    def create
+        @article = Article.new(article_params)
+
+        @article.save
+        redirect_to @article
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+        def article_params
+            params.require(:article).permit(:title, :text)
+        end
 end
