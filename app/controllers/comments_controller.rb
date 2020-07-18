f297281ec48a3cf2a995f1b946d25e9f6f82f7af
Gerald Todd <gerald@gtodd.dev>
2020-07-17 21:38:47 -0700

    Clean up `/app/views/articles/show.html.erb` by using comment partials.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,31 @@
 class CommentsController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def create
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.create(comment_params)
+
+        redirect_to article_path(@article)
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+        def comment_params
+            params.require(:comment).permit(:commenter, :body)
+        end
 end
