dc0436a126f5d90172c81a43fbe1ddfcbb2bcef3
Gerald Todd <gerald@gtodd.dev>
2020-07-17 20:52:30 -0700

    Add `delete` action and links for articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,6 +36,10 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to articles_path
     end
 
     private
